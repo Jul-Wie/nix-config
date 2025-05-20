80f3d04e3d09152482da3f4282c61fbf469ecb20
Jul-Wie <juulwie@protonmail.com>
2025-05-20 10:43:24 +0200

    Updated Nix Config on pc

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,7 @@ services = {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      jack.enable = true;
+#      jack.enable = true;
   };
   jack = {
     jackd.enable = true;
